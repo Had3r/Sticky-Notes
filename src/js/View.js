fa0b6de56e8d74171b239703d797a4af9d35627f
Adrian <adrian.zawadzki@outlook.com>
2019-02-09 12:43:40 +0100

    control commit

--- src/js/View.js
+++ src/js/View.js
@@ -1,10 +1,18 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
-const { pre } = hh(h);
+const { pre, div, h1, header, section } = hh(h);
 
 function view(msg, model) {
-    return pre(JSON.stringify(model, null, 3));
+	return div({ className: 'wrapper' }, [
+		h1(
+			{
+				className: 'btn',
+			}
+		),
+		pre(JSON.stringify(model, null, 3))
+	])
+
 }
 
 
