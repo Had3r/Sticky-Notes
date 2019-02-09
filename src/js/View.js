6691f2d0d9ecc12adf295719fe5aa419e9654b88
Adrian <adrian.zawadzki@outlook.com>
2019-02-09 15:04:24 +0100

    building btn

--- src/js/View.js
+++ src/js/View.js
@@ -1,18 +1,24 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
-const { pre, div, h1, header, section } = hh(h);
+const { pre, div, h1, button, span } = hh(h);
 
 function view(msg, model) {
 	return div({ className: 'wrapper' }, [
 		h1(
 			{
-				className: 'btn',
-			}
+				className: 'heading',
+			},
+			'Sticky Notes'
+		),
+		button(
+			{
+				className: 'btn'
+			},
+			span('Add Questions'),
 		),
 		pre(JSON.stringify(model, null, 3))
 	])
-
 }
 
 
